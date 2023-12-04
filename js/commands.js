314a25e76efb2b326734880dc4fe4447d7f497b8
Vishnu Samudrala <vv859047@gmail.com>
2023-12-04 18:59:13 +0530

    Added new files
    new files

---
+++ js/commands.js
@@ -0,0 +1,71 @@
+var youtube = "https://www.youtube.com/technicalvishnu/";
+var twitter = "https://www.twitter.com/vishnu2k04/";
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/forrestknight/";
+var email = 'mailto:forrest@fkcodes.com';
+var reddit = "https://www.reddit.com/user/OnlyMemer420/";
+
+intro = [
+    "<br>",
+    "Hey, I'm Vishnu! 👋",
+    "I'm a 19-year-old computer science student from India.",
+    "Passionate about Linux, video editing, and diving into DSA for college.",
+    "I enjoy working on small projects and have dabbled in creating YouTube videos.",
+    "Let's connect and embark on a journey to explore the tech world together! 🚀👨‍💻",
+    "<br>"
+]
+
+
+whoami = [
+        "<br>",
+        "You're the unsolved riddle wrapped in a mystery,",
+        "sprinkled with a dash of enigma",
+        "basically, the Sherlock Holmes of your own identity",
+        "<br>"
+];
+
+social = [
+    "<br>",
+    'youtube        <a href="' + youtube + '" target="_blank">youtube/technicalvishnu' + "</a>",
+    'twitter        <a href="' + twitter + '" target="_blank">twitter/vishnu2k04' + '</a>',
+    'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/techvishnu' + "</a>",
+    'instagram      <a href="' + instagram + '" target="_blank">instagram/vishnu2ko4' + '</a>',
+    'github         <a href="' + github + '" target="_blank">github/techvishnu' + "</a>",
+    'reddit         <a href="' + reddit + '" target="_blank">reddit/OnlyMemer420' + "</a>",
+    "<br>"
+];
+
+
+projects = [
+    "<br>",
+    "Coming Soon.",
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">whoami</span>         yea who are you?',
+    '<span class="command">intro</span>          Who is Vishnu?',    
+    '<span class="command">youtube</span>        My YouTube channel',
+    '<span class="command">social</span>         My Socials!!',
+    '<span class="command">projects</span>       some small projects',
+    '<span class="command">history</span>        Let\'s see what you\'ve been up to',
+    '<span class="command">help</span>           Even Saket knows what this does',
+    '<span class="command">clear</span>          breathe out',
+    '<span class="command">banner</span>         Display the header',
+    '<span class="command">email</span>          Never Spam me',
+    "<br>",
+];
+
+banner = [
+    '<span class="index">This is Termofolio </span>',
+    '<span class="color2">Welcome to my web terminal</span>',
+    "<span class=\"color2\">To summon the magical command list, simply cast</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+    "     ____________________  __   _    ___________ __  ___   ____  __",
+    "    /_  __/ ____/ ____/ / / /  | |  / /  _/ ___// / / / | / / / / /",
+    "     / / / __/ / /   / /_/ /   | | / // / \\__ \/ /_/ /  |/ / / / / ",
+    "    / / / /___/ /___/ __  /    | |/ // / ___/ / __  / /|  / /_/ /  ",
+    "   /_/ /_____/\____/_/ /_/     |___/___//____/_/ /_/_/ |_/\____/   ",
+    "                                                                    ",
+]
